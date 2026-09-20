6c6482856cb0f6fff4e53ab24c9418325be0d62d
mabdolalizadeh <m.abdolalizadeh@outlook.com>
2026-09-20 14:44:28 +0330

    ecosystem updated

--- harekat-backend/ecosystem.config.cjs
+++ harekat-backend/ecosystem.config.cjs
@@ -10,11 +10,11 @@ module.exports = {
       max_memory_restart: '500M',
       env: {
         NODE_ENV: 'development',
-        PORT: 3000
+        PORT: 80
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 80
       },
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       error_file: 'logs/pm2-err.log',
